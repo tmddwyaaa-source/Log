ca89c045d2884990199db729be16270bcb23efbc
tmddwy <tmddwyaaa@gmail.com>
2025-12-07 16:39:14 +0800

    Add files via upload

---
+++ 经典冒泡排序.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+//经典冒泡排序
+int main()
+{
+	int arr[10] = { 1,3,4,5,2,7,9,10,6,8 };
+    int i = 0;
+    for (i = 0; i < 10; i++)
+    {
+        int j = 0;
+        for (j = 0; j < 10-i; j++)
+        {
+            if (arr[j+1] > arr[j])
+            {
+                int x = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = x;
+            }
+        }
+    }
+	for (i = 0; i < 10; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
