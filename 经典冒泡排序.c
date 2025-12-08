713d4a8273953d51ce52f466da34ff6fe75073cc
tmddwy <tmddwyaaa@gmail.com>
2025-12-08 10:50:41 +0800

    Update 经典冒泡排序.c

--- 经典冒泡排序.c
+++ 经典冒泡排序.c
@@ -4,6 +4,7 @@ int main()
 {
 	int arr[10] = { 1,3,4,5,2,7,9,10,6,8 };
     int i = 0;
+	int flag = 1;
     for (i = 0; i < 10; i++)
     {
         int j = 0;
@@ -14,8 +15,12 @@ int main()
                 int x = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = x;
+				flag = 0;
             }
         }
+		if(flag == 1)
+		{
+			break;
     }
 	for (i = 0; i < 10; i++)
     {
@@ -23,3 +28,4 @@ int main()
     }
     return 0;
 }
+
