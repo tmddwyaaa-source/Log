c22024f74d9b1971d16c3100327937f8764133c2
tmddwy <tmddwyaaa@gmail.com>
2025-12-19 22:20:29 +0800

    Add files via upload

---
+++ 枚举和联合.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+enum dey
+{
+  Mon,//默认初始值是0,枚举都是有默认值的
+  Tues,//连续的存储，0 1 2 3 4 5 6
+  Wed,
+  Thur,
+  Fri,
+  Sat,
+  Sun
+};
+
+enum shuz
+{
+    Yi=1,//如果改第一位枚举常量的初始值，如0变成1
+    Er,
+    San,
+    Si,
+    Wu
+};
+int main()
+
+{
+    enum day n = Fri;//enum枚举只有实例化变量才占空间
+    printf("%d\n", Mon);
+    printf("%d\n", Tues);
+    printf("%d\n", Wed);
+    printf("%d\n", Thur);
+    printf("%d\n", Fri);
+    printf("%d\n", Sat);
+    printf("%d\n", Sun);
+
+    printf("\n\n%d\n", Yi);
+    printf("%d\n", Er);
+    printf("%d\n", San);
+    printf("%d\n", Si);
+    printf("%d\n", Wu);
+    return 0;
+}
