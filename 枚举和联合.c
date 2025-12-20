02382bcaac5939213a4f0d3b643b59613963d191
tmddwy <tmddwyaaa@gmail.com>
2025-12-20 21:32:05 +0800

    Update 枚举和联合.c

--- 枚举和联合.c
+++ 枚举和联合.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 enum dey
 {
   Mon,//默认初始值是0,枚举都是有默认值的
@@ -18,9 +17,25 @@ enum shuz
     Si,
     Wu
 };
+union un//联合体
+{
+    int a;
+    char c;
+}u;
+int cheak()
+{
+    union man
+    {
+        char c;
+        int a;
+    }m;
+    m.a = 1;
+    return m.c;
+}
 int main()
 
 {
+    printf("结构体\n");
     enum day n = Fri;//enum枚举只有实例化变量才占空间
     printf("%d\n", Mon);
     printf("%d\n", Tues);
@@ -35,5 +50,21 @@ int main()
     printf("%d\n", San);
     printf("%d\n", Si);
     printf("%d\n", Wu);
+    printf("联合体\n");//他们的内存是有公用的部分的
+    union un u;
+    printf("%d\n", sizeof(u));
+    printf("%d\n", sizeof(u.a));
+    printf("%d\n", sizeof(u.c));
+
+    printf("联合体判断大端小端\n");
+    int x = cheak();
+    printf("%d\n", x);
+    if (x == 1)
+    {
+        printf("是小端\n");
+    }
+    else
+        printf("是大端\n");
+
     return 0;
 }
