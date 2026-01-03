2b33d7e5390ebcfb48e9fd55d002ac6409b080db
tmddwy <tmddwyaaa@gmail.com>
2026-01-03 19:25:16 +0800

    Update 练习2.java

--- 练习2.java
+++ 练习2.java
@@ -7,7 +7,10 @@ public class Jiemian {
     {
         guanlisk gl = new guanlisk();
         Scanner sc=new Scanner(System.in);
-        while(n)
+        boolean z=true;
+        Shuku shuku=new Shuku();
+        gl.glisk();
+        do
         {
             IO.println("1.展示图书");
             IO.println("2.添加图书");
@@ -17,24 +20,41 @@ public class Jiemian {
             {
                 case 0:
                     IO.println("退出");
+                    z=false;
                     break;
                 case 1:
                     gl.dayinsk();
                     break;
 
                 case 2:
+                    IO.println("请输入书名");
+                    shuku.bookname=sc.next();
+                    IO.println("请输入作者");
+                    shuku.zuoz=sc.next();
+                    IO.println("请输入价格");
+                    shuku.jiage=sc.nextInt();
+                    boolean flag=gl.addsk(shuku);
+                    if(flag)
+                    {
+                        IO.println("添加成功");
+                    }
+                    else
+                    {
+                        IO.println("添加失败");
+                    }
                     break;
                 default:
                     IO.println("输入错误，请重试");
             }
-        }
+        }while(z);
     }
 }
 package tushu;
 
+
 public class guanlisk {
     Shuku[] shuk=new Shuku[50];
-    public void guanlisk()
+    public void glisk()
     {
         Shuku shuku=new Shuku();
         shuku.bookname="平凡的世界";
@@ -59,7 +79,20 @@ public class guanlisk {
             }
         }
     }
-
+    public boolean addsk(Shuku sk)
+    {
+        boolean flag=false;
+        for(int i=0;i<shuk.length;i++)
+        {
+            if(shuk[i]==null)
+            {
+                   shuk[i]=sk;
+                   flag=true;
+                   break;
+            }
+        }
+        return flag;
+    }
 }
 package tushu;
 
