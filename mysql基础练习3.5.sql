52c7688aa3a16e4c1d446b1c032d092872efa8fd
tmddwy <tmddwyaaa@gmail.com>
2026-05-15 20:49:22 +0800

    Add files via upload

---
+++ mysql基础练习3.5.sql
@@ -0,0 +1,18 @@
+create database fbl default character set utf8; 
+use fbl;
+alter table huizhang add column lianxi2 int default 0;
+#添加新列并修改默认值		#ALTER TABLE 表名 ADD COLUMN 列名 类型名 DEFAULT 默认值;
+insert into huizhang value(default,'王五','十点',default); #测试默认值
+insert into bydengji value(default,"老六",27,'男');
+
+update bydengji set nianl_sb=16 where id_sb=2344; 
+#更新数据 UNDATA 表名 SET 列名 = 数据 WHERE 条件
+# 注：where 是限制更新条件的否则会导致所有数据被修改
+
+delete from bydengji where id_sb=2342;
+#删除指定数据,where是条件,诺无条件 则该表所有数据清空。逐条性清理数据,会记录(有日志),效率较低
+#DELETE FROM 表名 WHERE 条件;   注：修改非主键或索引位会有安全模式阻止删除
+
+truncate table huizhang;
+#清空表数据 TRUNCATE TABLE 表名;
+#整体性清空表数据,不记录，无日志,效率较高
